1d361b20f4c7c1f59c1e62477094252ca68f55a8
yash rawat <yashrawat945@gmail.com>
2024-07-16 15:42:25 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = item
+        } 
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    };
+}
