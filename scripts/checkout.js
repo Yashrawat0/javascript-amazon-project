bfad8e2449d4443038520960e62e15c5abc60b92
yash rawat <yashrawat945@gmail.com>
2024-07-17 13:00:44 +0530

    delete porducts form HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((carItem) => {
     })
     
     cartSummaryHTML += `
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,7 +100,12 @@ document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML
 document.querySelectorAll(".js-delete-link").forEach((link) => {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId;
+
         removeFromCart(productId);
-        console.log(cart)
+
+        const container = document.querySelector(
+            `.js-cart-item-container-${productId}`
+        );
+        container.remove()
     });
 });
