8625ed727c90035b03a7cdec94aa119f8d9f8b8b
yash rawat <yashrawat945@gmail.com>
2024-07-19 17:27:16 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import "../data/cart-oop.js"  // this run the file without importing any thing.
 
 renderOrderSummary();
 renderPaymentSummary();
